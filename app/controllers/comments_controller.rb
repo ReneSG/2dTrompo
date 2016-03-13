1854b2872c650f8c7692bb598f85f9bd24810ebc
Eduardo Trujillo <ed.trujillo.r@outlook.com>
2016-03-12 22:14:38 -0600

    Comments controller fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,74 @@
 class CommentsController < ApplicationController
-  
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
+  # GET /comments
+  # GET /comments.json
   def index
-    respond_with Comment.all
+    @comments = Comment.all
   end
 
+  # GET /comments/1
+  # GET /comments/1.json
   def show
-    respond_with Comment.find(params[:id])
   end
 
+  # GET /comments/new
+  def new
+    @comment = Comment.new
+  end
+
+  # GET /comments/1/edit
+  def edit
+  end
+
+  # POST /comments
+  # POST /comments.json
   def create
-    respond_with Comment.create(params[:comment])
+    @comment = Comment.new(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PATCH/PUT /comments/1
+  # PATCH/PUT /comments/1.json
   def update
-    respond_with Comment.update(params[:id], params[:comment])
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # DELETE /comments/1
+  # DELETE /comments/1.json
   def destroy
-    respond_with Comment.destroy(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
-
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:user_id, :body, :rating, :taco_id)
+      params.require(:comment).permit(:author_id, :body, :rating, :taco_id)
     end
 end
