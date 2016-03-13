d353c41f5bbf6354c3a61c7abef5fdc62832d4dc
LewisErick <repos.zul@gmail.com>
2016-03-12 21:20:59 -0600

    Minor changes to app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,52 +1,5 @@
 var myApp = angular.module('2DTrompo', ['ui.router', 'templates'])
 
-myApp.controller('MainController', ['$scope', 'dataStorage', function($scope, dataStorage) {
-
-	$scope.user =	dataStorage.getCurrentUser()
-
-	$scope.markerClick = function() {
-		
-	}
-
-	$scope.getRating = function() {
-		console.log($scope.r1);
-		console.log($scope.r2);
-		console.log($scope.r3);
-		console.log($scope.r4);
-		console.log($scope.r5);
-		rating = $scope.r1 + $scope.r2 + $scope.r3 + $scope.r4 + $scope.r5;
-		return rating;
-	}
-
-	$scope.form_valid = function() {
-		if ($scope.taco == '') {
-			return false;
-		}
-	}
-
-	$scope.addTaco = function(){
-		pos = getPosition();
-		rating = getRating();
-		if(!form_valid()) { return; }
-	  	taco_id = dataStorage.tacos.create({
-	    	image: $scope.img,
-			name: $scope.taco,
-			lattitude: pos.lat,
-			longitude: pos.lng
-	  	});
-	  	dataStorage.comments.create({
-	  		user_id: $scope.user.id,
-	  		body: $scope.comment,
-	  		rating: rating,
-	  		taco_id: taco_id
-	  	})
-	  	$scope.taco = '';
-	 	$scope.img = '';
-	 	$scope.rating = '';
-	 	$scope.comment = '';
-	};
-}]);
-
 myApp.config( function($stateProvider, $urlRouterProvider) {
 
 	$stateProvider.state('home', {
@@ -61,6 +14,8 @@ myApp.config( function($stateProvider, $urlRouterProvider) {
 
 //MAPSCONTROLLER
 myApp.controller('MapsController', ['$scope', 'dataStorage', function($scope, dataStorage){
+
+	$scope.user =	dataStorage.getCurrentUser()
 
 	//Current position
 	var currentPos;
@@ -169,13 +124,55 @@ myApp.controller('MapsController', ['$scope', 'dataStorage', function($scope, da
 		$scope.markersUpdate;
 	}
 
+	$scope.markerClick = function() {
+		console.log("Click marker event.");
+	}
+
 	$scope.addOnClickToMarkers = function(){
 
 		for(var i = 0; i < usedMarkers.length; i++){
 			usedMarkers[i].OnClick = function(){
-				//ADD ON CLICK
+				markerClick();
 			}
 		}
 	}
+
+	$scope.getRating = function() {
+		console.log($scope.r1);
+		console.log($scope.r2);
+		console.log($scope.r3);
+		console.log($scope.r4);
+		console.log($scope.r5);
+		rating = $scope.r1 + $scope.r2 + $scope.r3 + $scope.r4 + $scope.r5;
+		return rating;
+	}
+
+	$scope.form_valid = function() {
+		if ($scope.taco == '') {
+			return false;
+		}
+	}
+
+	$scope.addTaco = function(){
+		pos = getPosition();
+		rating = getRating();
+		if(!form_valid()) { return; }
+	  	taco_id = dataStorage.tacos.create({
+	    	image: $scope.img,
+			name: $scope.taco,
+			lattitude: pos.lat,
+			longitude: pos.lng
+	  	});
+	  	dataStorage.comments.create({
+	  		user_id: $scope.user.id,
+	  		body: $scope.comment,
+	  		rating: rating,
+	  		taco_id: taco_id
+	  	})
+	  	$scope.taco = '';
+	 	$scope.img = '';
+	 	$scope.rating = '';
+	 	$scope.comment = '';
+	};
 
 }]);
