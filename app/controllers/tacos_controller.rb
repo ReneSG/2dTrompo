6f4ac69ba96809ec05633781c740ab9b8ab7b9de
Eduardo Trujillo <ed.trujillo.r@outlook.com>
2016-03-12 22:15:27 -0600

    Tacos controller fixed

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,29 +1,74 @@
 class TacosController < ApplicationController
+  before_action :set_taco, only: [:show, :edit, :update, :destroy]
 
+  # GET /tacos
+  # GET /tacos.json
   def index
-    respond_with Taco.all
+    @tacos = Taco.all
   end
 
+  # GET /tacos/1
+  # GET /tacos/1.json
   def show
-    respond_with Taco.find(params[:id])
   end
 
+  # GET /tacos/new
+  def new
+    @taco = Taco.new
+  end
+
+  # GET /tacos/1/edit
+  def edit
+  end
+
+  # POST /tacos
+  # POST /tacos.json
   def create
-    respond_with Taco.create(params[:taco])
+    @taco = Taco.new(taco_params)
+
+    respond_to do |format|
+      if @taco.save
+        format.html { redirect_to @taco, notice: 'Taco was successfully created.' }
+        format.json { render :show, status: :created, location: @taco }
+      else
+        format.html { render :new }
+        format.json { render json: @taco.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PATCH/PUT /tacos/1
+  # PATCH/PUT /tacos/1.json
   def update
-    respond_with Taco.update(params[:id], params[:taco])
+    respond_to do |format|
+      if @taco.update(taco_params)
+        format.html { redirect_to @taco, notice: 'Taco was successfully updated.' }
+        format.json { render :show, status: :ok, location: @taco }
+      else
+        format.html { render :edit }
+        format.json { render json: @taco.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # DELETE /tacos/1
+  # DELETE /tacos/1.json
   def destroy
-    respond_with Taco.destroy(params[:id])
+    @taco.destroy
+    respond_to do |format|
+      format.html { redirect_to tacos_url, notice: 'Taco was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_taco
+      @taco = Taco.find(params[:id])
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def taco_params
-      params.require(:taco).permit(:name, :image, :rating, :lattitude, :longitude)
+      params.require(:taco).permit(:name, :image, :rating, :comment_id, :lattitude, :longitude)
     end
 end
